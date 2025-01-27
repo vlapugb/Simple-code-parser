e1f5337d3f658682bf8349797e9885499cb4a98a
vlapugb <vladimirpugovkinsbp@gmail.com>
2025-01-28 01:57:13 +0300

    fix: simple errors

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
+use clap::Parser;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+use pyo3::prelude::*;
+mod modules;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(
+        long = "filename",
+        help = "Set path-file from root of the project to parse it"
+    )]
+    name: String,
+}
 
 #[macro_use]
 extern crate lalrpop_util;
 
-use crate::parse::to_dot;
+use crate::modules::dot_tree::to_dot;
+use crate::modules::graphviz::plotting_graphviz;
 
-lalrpop_mod!(#[allow(clippy::all)] grammar);
-
-mod ast;
-mod parse;
+lalrpop_mod!(
+    #[allow(clippy::all)]
+    grammar
+);
 
 fn main() {
+    pyo3::prepare_freethreaded_python();
+
+    let args = Args::parse();
+    let file_path = Path::new(args.name.as_str());
+    let mut file = fs::File::open(file_path).expect("File not found");
+    let mut content = String::new();
+
     let parser = grammar::ProgramGrammarParser::new();
-    let source = "let x = 42+3*5 let y = x+10 let z = (y*3) + (x+2) print z let a = (z+10)*(2+3) let b = (a+(x+3)*z) print b
-";
+    let _ = file.read_to_string(&mut content);
 
-
-    match parser.parse(source) {
+    match parser.parse(content.as_str()) {
         Ok(stmts) => {
             let dot_tree = to_dot(&stmts);
+            plotting_graphviz(dot_tree.as_str(), "ast.png").expect("Failed to plot graph");
             println!("Parsed: {}", dot_tree)
-        },
+        }
         Err(e) => println!("Error: {:?}", e),
     }
 
-}
 
+}
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-    use std::io::{self, Read};
-    use std::path::Path;
     use super::*;
     #[test]
     fn test_grammar_parser_stmt1() {
@@ -42,10 +61,7 @@ mod tests {
         let source1 = "let x = 42";
         let parsed_statements = parser1.parse(source1).expect("Parsing failed");
         let dot_representation = to_dot(&parsed_statements);
-        assert_eq!(
-            dot_representation,
-            content,
-        );
+        assert_eq!(dot_representation, content,);
     }
     #[test]
     fn test_grammar_parser_stmt2() {
@@ -57,10 +73,7 @@ mod tests {
         let source2 = "print x";
         let parsed_statements = parser2.parse(source2).expect("Parsing failed");
         let dot_representation = to_dot(&parsed_statements);
-        assert_eq!(
-            dot_representation,
-            content,
-        );
+        assert_eq!(dot_representation, content,);
     }
     #[test]
     fn test_grammar_parser_stmt3() {
@@ -72,10 +85,7 @@ mod tests {
         let source3 = "let z = (y + 6) * 2";
         let parsed_statements = parser3.parse(source3).expect("Parsing failed");
         let dot_representation = to_dot(&parsed_statements);
-        assert_eq!(
-            dot_representation,
-            content,
-        );
+        assert_eq!(dot_representation, content,);
     }
     #[test]
     fn test_grammar_parser_stmt4() {
@@ -87,9 +97,6 @@ mod tests {
         let source4 = "let x = 42+3*5 let y = x+10 let z = (y*3) + (x+2) print z let a = (z+10)*(2+3) let b = (a+(x+3)*z) print b";
         let parsed_statements = parser4.parse(source4).expect("Parsing failed");
         let dot_representation = to_dot(&parsed_statements);
-        assert_eq!(
-            dot_representation,
-            content,
-        );
+        assert_eq!(dot_representation, content,);
     }
 }
